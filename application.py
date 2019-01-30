32617afd0598d85fca41972da4db9e983aac4285
Kumar Ayush <142ayushkumar@gmail.com>
2019-01-30 23:46:46 +0530

    signout done and other minor improvements

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, redirect, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,50 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if 'username' in session:
+        return render_template("index.html", username=session["username"])
+    else:
+        print("here1")
+        return redirect("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    print("check")
+    if 'username' in session:
+        return redirect("/")
+    else:
+        print(request.method)
+        if "log-in" in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            login = 'Username or Password is incorrect'
+            print("error source")
+            if db.execute("SELECT * FROM users WHERE username= :username AND password= :password", {"username": username, "password": password}).rowcount == 0:
+                return render_template("login.html", login=login, error=True)
+            else:
+                session['username'] = username
+                return redirect("/")
+        elif "sign-up" in request.form:
+            print("here11")   
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            if len(password) == 0 or len(username) == 0:
+                signup = 'password field can\'t be blank'
+                return render_template("login.html", signup=signup, error1=True)
+            try:
+                db.execute("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)", {"email":email, "username":username, "password":password})
+                db.commit() 
+                session['username'] = username
+                return redirect("/")
+            except:
+                signup = 'username or email exists, please try different username and/or email'
+                return render_template("login.html", signup=signup, error1=True)
+        else:
+            return render_template("login.html")
+
+
+@app.route("/signout")
+def signout():
+    session.clear()
+    return redirect("/")
